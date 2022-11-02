2627a027911d9a8039e4b5fa49244e37e16fc959
devmnj <manojap@outlook.com>
2022-11-02 09:01:02 +0530

    feat: ✨

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -13,8 +13,7 @@ export async function GET({url}) {
 
   `); 
   return new Response(
-    `
-    <?xml version="1.0" encoding="UTF-8" ?>
+    `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset
       xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xhtml="https://www.w3.org/1999/xhtml"
