cee2fcb417952194b860bb795a831b74609248c7
devmnj <manojap@outlook.com>
2022-11-02 19:47:07 +0530

    :lock:

--- src/lib/Search2.svelte
+++ src/lib/Search2.svelte
@@ -11,7 +11,7 @@
  
   <form method="GET">
     <div class="relative text-white-600 rouded focus-within:text-yellow-100">
-      <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+      <span name='search_btn_container' class="absolute inset-y-0 left-0 flex items-center pl-2">
         <button name="search-btn" type="submit" class="p-1 focus:outline-none focus:shadow-outline">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
